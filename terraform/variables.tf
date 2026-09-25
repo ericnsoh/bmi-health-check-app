e83a0457f0553871f34b4a433dd635d06480d29b
Eric Nsoh <nsoheric88@gmail.com>
2026-09-25 00:18:57 -0400

    updated github

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   type        = string
   description = "AWS region for all resources"
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "project_name" {
